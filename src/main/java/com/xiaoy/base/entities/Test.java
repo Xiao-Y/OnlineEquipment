811474918da57a12a140c5775aeff0fb5a0c8203
Administrator <Administrator@liuyongtao>
2015-07-30 10:28:49 +0800

    修改hibernate为注解形式。修正配置文件的位置。分割Spring-Hibernate的文件为tx和druid

--- src/main/java/com/xiaoy/base/entities/Test.java
+++ src/main/java/com/xiaoy/base/entities/Test.java
@@ -1,38 +1,45 @@
 package com.xiaoy.base.entities;
 
-public class Test
-{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "TEST")
+public class Test {
 	private int id;
 	private String name;
 	private String sex;
 
-	public int getId()
-	{
+	@Id
+	@GenericGenerator(name = "generator", strategy = "native")
+	@Column(name = "id", unique = true, nullable = false)
+	public int getId() {
 		return id;
 	}
 
-	public void setId(int id)
-	{
+	public void setId(int id) {
 		this.id = id;
 	}
 
-	public String getName()
-	{
+	@Column(name = "name", length = 50)
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getSex()
-	{
+	@Column(name = "sex", length = 2)
+	public String getSex() {
 		return sex;
 	}
 
-	public void setSex(String sex)
-	{
+	public void setSex(String sex) {
 		this.sex = sex;
 	}
 }
