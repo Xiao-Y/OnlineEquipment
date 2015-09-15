280d35e58e8797d59b5575252de8685fa012ef15
Xiao-Y <lyongtao123@126.com>
2015-09-15 22:36:19 +0800

    添加用户头像上传，未完成，准备集成插件

--- src/main/webapp/background/usermsg/user/app/view/UserAdd.js
+++ src/main/webapp/background/usermsg/user/app/view/UserAdd.js
@@ -4,119 +4,133 @@ Ext.define("AM.view.UserAdd",{
 	id : "userAddWindow",
 	modal : true,
 	width : 570,
-	height : 300,
+	height : 320,
 	layout : "fit",
 	items : [{
-		id : "userAddForm",
-		xtype : 'form',
-		buttonAlign : 'right',
-		bodyPadding : 15,
-		autoScroll : true,
-		layout : 'anchor',
-		defaults : {
-			labelWidth : 130,
-			labelAlign : 'right',
-			anchor : '90%'
-		},
-		defaultType : 'textfield',
+		layout : 'column',
 		items : [{
-			xtype : 'hiddenfield',
-			name : 'id'
-		}, {
-			fieldLabel : '用户名',
-			name : 'username',
-			allowBlank : false
-		}, {
-			fieldLabel : '密码',
-			inputType : 'password',// radio,text,password,file
-			emptyText : '密码输入',// 当为空时显示的内容
-			value : "111111",
-			name : 'password',
-			allowBlank : false
-		}, {
-			fieldLabel : '出生年月',
-			xtype : 'datefield',
-			format : 'Y-m-d',
-			invalidText : "{0}不是一个正确的日期格式，如2015-08-06",
-			name : 'birthday'
-		}, {
-			fieldLabel : '角色',
-			xtype : 'combobox',
-			displayField : 'roleName',
-			allowBlank : false,
-			multiSelect : true,//允许多选
-			valueField : 'id',
-			queryMode : 'local',
-			forceSelection : true,// 所选择的值必须是列表中的值
-			store : 'RoleStore',
-			name : 'roleId',
-			id : 'roleId',
-			listConfig : {// 下拉列表的样式
-				emptyText : "<font color='red'>没有找到匹配项</font>"
-			}
-		}, {
-			fieldLabel : '省市/直辖市',
-			xtype : 'combobox',
-			displayField : 'name',
-			allowBlank : false,
-			valueField : 'id',
-			queryMode : 'local',
-			forceSelection : true,// 所选择的值必须是列表中的值
-			store : 'ProvinceStore',
-			id : 'province',
-			name : 'province',
-			listConfig : {// 下拉列表的样式
-				emptyText : "<font color='red'>没有找到匹配项</font>"
+			columnWidth : .3, // 该列有整行中所占百分比
+           	layout : "fit", // 从上往下的布局
+           	items : [{
+				xtype : "panel",
+				height : 300,
+				html : "<b>34343</b>"
+           	}]
+		},{
+			columnWidth : .7, // 该列有整行中所占百分比
+			id : "userAddForm",
+			xtype : 'form',
+			buttonAlign : 'right',
+			bodyPadding : 15,
+			autoScroll : true,
+			border:false,
+			layout : 'form',
+			defaults : {
+				labelWidth : 130,
+				labelAlign : 'right',
+				anchor : '90%'
 			},
-			listeners: {
-        		select:function(combo){
-        			Ext.getCmp('city').store.removeAll();
-        			Ext.getCmp('area').store.removeAll();
-    	   			Ext.getCmp('city').store.load({
-    	   				params : {
-    	   					province:combo.getValue()
-    	   				}
-    	   			});// 刷新下拉框
-        		}
-        	}
-		}, {
-			fieldLabel : '市/直辖县级',
-			xtype : 'combobox',
-			displayField : 'name',
-			allowBlank : false,
-			valueField : 'id',
-			queryMode : 'local',
-			forceSelection : true,// 所选择的值必须是列表中的值
-			store : 'CityStore',
-			id : 'city',
-			name : 'city',
-			listConfig : {// 下拉列表的样式
-				emptyText : "<font color='red'>没有找到匹配项</font>"
-			},
-			listeners: {
-        		select:function(combo){
-        			Ext.getCmp('area').store.removeAll();
-    	   			Ext.getCmp('area').store.load({
-    	   				params : {
-    	   					city:combo.getValue()
-    	   				}
-    	   			});// 刷新子模块下拉框
-        		}
-        	}
-		}, {
-			fieldLabel : '区/县',
-			xtype : 'combobox',
-			displayField : 'name',
-			allowBlank : false,
-			valueField : 'id',
-			queryMode : 'local',
-			forceSelection : true,// 所选择的值必须是列表中的值
-			store : 'AreaStore',
-			id : 'area',
-			name : 'area',
-			listConfig : {// 下拉列表的样式
-				emptyText : "<font color='red'>没有找到匹配项</font>"
-			}
+			defaultType : 'textfield',
+			items : [{
+				xtype : 'hiddenfield',
+				name : 'id'
+			}, {
+				fieldLabel : '用户名',
+				name : 'username',
+				allowBlank : false
+			}, {
+				fieldLabel : '密码',
+				inputType : 'password',// radio,text,password,file
+				emptyText : '密码输入',// 当为空时显示的内容
+				value : "111111",
+				name : 'password',
+				allowBlank : false
+			}, {
+				fieldLabel : '出生年月',
+				xtype : 'datefield',
+				format : 'Y-m-d',
+				invalidText : "{0}不是一个正确的日期格式，如2015-08-06",
+				name : 'birthday'
+			}, {
+				fieldLabel : '角色',
+				xtype : 'combobox',
+				displayField : 'roleName',
+				allowBlank : false,
+				multiSelect : true,//允许多选
+				valueField : 'id',
+				queryMode : 'local',
+				forceSelection : true,// 所选择的值必须是列表中的值
+				store : 'RoleStore',
+				name : 'roleId',
+				id : 'roleId',
+				listConfig : {// 下拉列表的样式
+					emptyText : "<font color='red'>没有找到匹配项</font>"
+				}
+			}, {
+				fieldLabel : '省市/直辖市',
+				xtype : 'combobox',
+				displayField : 'name',
+				allowBlank : false,
+				valueField : 'id',
+				queryMode : 'local',
+				forceSelection : true,// 所选择的值必须是列表中的值
+				store : 'ProvinceStore',
+				id : 'province',
+				name : 'province',
+				listConfig : {// 下拉列表的样式
+					emptyText : "<font color='red'>没有找到匹配项</font>"
+				},
+				listeners: {
+	        		select:function(combo){
+	        			Ext.getCmp('area').getStore().removeAll();
+	        			Ext.getCmp('area').setValue("");
+	        			Ext.getCmp('city').getStore().removeAll();
+	    	   			Ext.getCmp('city').getStore().load({
+	    	   				params : {
+	    	   					province:combo.getValue()
+	    	   				}
+	    	   			});// 刷新下拉框
+	        		}
+	        	}
+			}, {
+				fieldLabel : '市/直辖县级',
+				xtype : 'combobox',
+				displayField : 'name',
+				allowBlank : false,
+				valueField : 'id',
+				queryMode : 'local',
+				forceSelection : true,// 所选择的值必须是列表中的值
+				store : 'CityStore',
+				id : 'city',
+				name : 'city',
+				listConfig : {// 下拉列表的样式
+					emptyText : "<font color='red'>没有找到匹配项</font>"
+				},
+				listeners: {
+	        		select:function(combo){
+	        			Ext.getCmp('area').getStore().removeAll();
+	    	   			Ext.getCmp('area').getStore().load({
+	    	   				params : {
+	    	   					city:combo.getValue()
+	    	   				}
+	    	   			});// 刷新子模块下拉框
+	        		}
+	        	}
+			}, {
+				fieldLabel : '区/县',
+				xtype : 'combobox',
+				displayField : 'name',
+				allowBlank : false,
+				valueField : 'id',
+				queryMode : 'local',
+				forceSelection : true,// 所选择的值必须是列表中的值
+				store : 'AreaStore',
+				id : 'area',
+				name : 'area',
+				listConfig : {// 下拉列表的样式
+					emptyText : "<font color='red'>没有找到匹配项</font>"
+				}
+			}]
 		}]
 	}],
 	dockedItems : [ {
